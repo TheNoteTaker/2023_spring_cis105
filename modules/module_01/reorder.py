232c77670d39e89a91a7e83687acf9a4d569a832
DevGuyRash <devguyrash@gmail.com>
2023-01-25 07:42:14 -0700

    Moved reorder.py to /bin

--- modules/module_01/reorder.py
+++
@@ -1,9 +0,0 @@
-import subprocess
-
-FILENAME = "./temp_definitions.md"
-
-with open(FILENAME, 'r', encoding='utf-8') as file:
-    definitions = "\n- ".join(sorted([line.lstrip() for
-                                        line in file.read().split("\n-")]))
-
-print(definitions)
